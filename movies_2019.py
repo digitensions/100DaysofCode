65d84b091a1f5169b420528ff777ed9208abe41c
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-07-06 14:21:27 +0100

    Rehash of script so it makes more sense
    Incorporated more comments and relocated last section so it makes more sense to script progression

--- movies_2019.py
+++ movies_2019.py
@@ -6,7 +6,7 @@ Asks you to enter a film you'd like to check, then lists the item and it's rank 
 If not, asks if you'd like to see the complete list.
 Asks finally if you'd like to restart the process by checking another title or not.
 '''
-
+# Dictionary containing separate entries for each film, followed by the rank index, eg [50]
 films = {
     "The Mule": [50],
     "The Favourite": [49],
@@ -59,42 +59,44 @@ films = {
     "Parasite": [2],
     "The Souvenir": [1]
 }
-
+# Exit command variable restarts the script at this point, when called at base of script by while loop
 exit_command = True
+# Introduction to what the script does, and how user can participate. Their film populates 'choice' variable
 def main():
-    # Welcome / intro statements
     print("\nHello! welcome to the BFI 'Films of 2019' checker\n")
     print("You can use this script to check whether your favourite film of 2019")
     print("made it to their top 50 and which position the BFI gave it...\n")
-    # This is the first question asked of the user using string input, and puts it in choice variable
-    # Uses .strip() to removes leading and trailing characters and .title() to alphabetize the first letter, matching dictionary
     choice = str(input("Which film do you want to check?: ")).strip().title()
+# if the choice is correctly typed (case of text accounted for in .title() formatting) the film rank is listed from the dictionary above
     if choice in films:
         print("\nThe film",choice,"is in the top 50, positioned at number",films[choice][0])
     else:
         print("\nYour film choice doesn't appear in the list, what a shame :(\n")
-    
-    # This next variable input asks if you'd like to view the complete dictionary
-    answer = str(input("\nWould you like to see the complete list of films? [y/n]: ")).strip().lower()
-    if answer[0] == "y":
-        print("\nThe BFI's top 50 films for 2019 and their ranking: \n")
-        for i in films:
-            print(i,films[i])
-    elif answer[0] == "n":
-        print("\nOkay!")
-    else:
-        print("I'm afraid you didn't give a valid answer. Let's start over!")
-        
-    # Final question asks if the user would like to try another films (probably pointless if they've seen the list!)
+# Input request do they want to try again (in case of excess enthusiasm or mis-typed first attempt)
     try_again = str(input("\nWould you like to try checking another film title? [y/n]: ")).strip().lower()
     if try_again[0] == "n":
-        print("\nThank you so much for trying this BFI 'Film of 2019' checker. Good bye!\n")
-        quit()
+# If no, they can see a list of all the films and exit quickly
+        answer = str(input("\nWould you like to see the complete list of films? [y/n]: ")).strip().lower()
+        if answer[0] == "y":
+            print("\nThe BFI's top 50 films for 2019 and their ranking: \n")
+# For loop to list all the items in dictionary 'films' followed by thank you and quit() exits the script
+            for i in films:
+                print(i,films[i])
+            print()
+            print("Thanks so much for trying out this BFI 'Film of 2019' checker. Good bye!\n")
+            quit()
+ # A no answer assumes they've had enough and want to move on, so thanks and rapid exit provided
+        elif answer[0] == "n":
+            print("\nThank you so much for trying this BFI 'Film of 2019' checker. Good bye!\n")
+            quit()
+ # Allows for a bad typing day
+        else:
+            print("I'm afraid you didn't give a valid answer. Let's start over!")
     elif try_again[0] == "y":
         print("\nOkay, let's try again! Good luck :)\n********************************************************")
     else:
         print("I'm afraid you didn't give a valid answer. Let's start over!")        
-
-# calls a loop that returns back to the start of main() unless "n" activates quit() above
+        
+# This while command calls exit_command variable True at top and restarts main().
 while exit_command:
     main()

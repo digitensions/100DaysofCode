f9d7712225f07d8b192f2d8dede5bb13241a25b5
digitensions <digitensions@gmail.com>
2020-07-10 16:16:55 +0100

    Uploading first test modules with comments

---
+++ module_one.py
@@ -0,0 +1,9 @@
+def avg(*list_numbers):
+    total = 0
+    for num in list_numbers:
+        if isinstance(num, (int, float)):
+            total += num
+        else:
+            raise TypeError("Wrong input data. Please make sure that everything is a number.")
+    return total /len(list_numbers)
+ 
---
+++ tests/test_module_one.py
@@ -0,0 +1,33 @@
+'''
+Easy and Medium unit tests for simple averaging script
+Imports unittest and function from tested module
+Creates class and calls unittest.TestCase which comes with Python standard library 'Assertion' unit tests
+An easy test would just check the sum works using correct inputs
+A medium test checks for the function working when an incorrect input is entered (string, not int or float)
+A test for module should always be named with prefix 'test_' and placed in folder called tests/
+Do not have more than one test module per module.
+'''
+
+import unittest
+from module_one import avg
+
+class EasyTestCase(unittest.TestCase):
+
+    def test_easy_input(self):
+        self.assertEqual(avg(1,2,3), 2)
+
+    def test_easy_input_two(self):
+        self.assertEqual(avg(10,10,10,10,10), 10)
+
+class MediumTestCase(unittest.TestCase):
+
+    def test_medium(self):
+        with self.assertRaises(TypeError):
+            self.assertEqual(avg(1,2,3,'Joanna'), 2)
+
+    def test_medium_two(self):
+        with self.assertRaises(TypeError):
+            self.assertEqual(avg(1,2,3,'5'), 2)
+
+if __name__ == '__main__':
+    unittest.main()

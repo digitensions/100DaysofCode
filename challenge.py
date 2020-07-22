39821de4d54b3e9deb0f24efbde03c875322d9cd
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-07-22 12:19:54 +0100

    Add shebang

--- challenge.py
+++ challenge.py
@@ -1,8 +1,10 @@
-"""
+#!/usr/bin/env python3
+
+'''
 Coding Challenge Skeleton #1
 This counter function purpose is to count how many english letters does your name contain.
 After writing your tests, develop the counter function as needed to pass all your tests.
-"""
+'''
 
 
 def counter(name):

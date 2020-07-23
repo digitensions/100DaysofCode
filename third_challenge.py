65d6585d65cd23a0a1397179e8f06f195f58938a
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-07-23 12:01:56 +0100

    Create third_challenge.py

---
+++ third_challenge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Coding Challenge Skeleton #3
+"""
+
+class Printer:
+    def __init__(self):
+        self._print = ""
+
+    def set_value(self, value):
+        self._print = value
+
+    def print_value(self):
+        print(self._print)

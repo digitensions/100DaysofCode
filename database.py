c707e5988cb5e7188db8142f52d28a6172501a5b
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-07-31 17:04:12 +0100

    Upload of database script
    Acccompanying test_database.py in tests folder.

---
+++ database.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+
+'''
+A short script that connects to training database and access Dictionary table
+Import mysql.connector very problematic in Linux and MacOS.
+Only successful way found to pip install into VENV on MacOs and run from within the VENV.
+Made it user interactive so it asks for a word to be supplied before information returned.
+'''
+
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+cursor = con.cursor()
+
+def main():
+    word = str(input("\nPlease enter your own word: "))
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
+    results = cursor.fetchall()
+
+    if results:
+        for result in results:
+            answer = result[1]
+        print("The word you have chosen '{0}' has the following definitions: {1}".format(word, answer))
+    else:
+        print("Sorry, your word is not found")
+
+if __name__ == "__main__":
+    main()

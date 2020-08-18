372e8834e9e7ad10d4aa75f30bf1e90b0fedc8de
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-08-18 13:51:27 +0100

    Example of custom made exceptions

---
+++ user_exceptions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+'''
+A short guessing game that uses custom built exceptions as classes
+to correct a users guesses until they find the specific number value.
+'''
+
+# define Python user-defined exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class ValueTooSmallError(Error):
+    """Raised when the input value is too small"""
+    pass
+
+class ValueTooLargeError(Error):
+    """Raised when the input value is too large"""
+    pass
+
+# you need to guess this number
+number = 10
+
+# user guesses a number until they gets it right
+while True:
+    try:
+        i_num = int(input("Enter a number: "))
+        if i_num < number:
+            raise ValueTooSmallError
+        elif i_num > number:
+            raise ValueTooLargeError
+        break
+    except ValueTooSmallError:
+        print("This value is too small, try again!")
+        print()
+    except ValueTooLargeError:
+        print("This value is too large, try again!")
+        print()
+
+print("Congratulations! You guessed it correctly.")
